37cc3eb6fa7303bf436fde77f69d33533d13e602
bluewolfJ124 <bluewolfJ124@gmail.com>
2024-12-25 15:13:10 +1000

    update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
     import Footer from "./footer.svelte"
     import { Heading, P, A } from 'flowbite-svelte';
     import { Button } from 'flowbite-svelte';
-    let imgClass = "inline-block flex-none w-[480px] h-[324px] bg-gradient-to-tr from-primary-500 to-secondary-800 rounded-lg transition-all duration-200 justify-center items-center p-1";
+    let imgClass = "inline-block flex-none sm:w-[240px] sm:h-[162px] lg:w-[480px] lg:h-[324px] bg-gradient-to-tr from-primary-500 to-secondary-800 rounded-lg transition-all duration-200 justify-center items-center p-1";
     let container;
     function syncScroll() {
       if (typeof window !== 'undefined') {
-        container.scrollLeft = window.scrollY;
+        container.scrollLeft = window.scrollY-600;
       }
     }
     import { onMount, onDestroy } from 'svelte';
@@ -25,7 +25,7 @@
   
     let count = $state(0);
     onMount(async () => {
-      const response = await fetch('https://counter.bluewolfj124.workers.dev/read'); // Replace with your API endpoint
+      const response = await fetch('https://counter.bluewolfj124.workers.dev/read');
       if (!response.ok) {
         console.log('Error fetching counter: ${response.status}');
       }
@@ -34,14 +34,14 @@
     });
     const fetchData = async () => {
     try {
-      const response = await fetch('https://counter.bluewolfj124.workers.dev/increment'); // Replace with your actual API endpoint
+      const response = await fetch('https://counter.bluewolfj124.workers.dev/increment'); 
       if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
       }
       count++;
       console.log('Request successful');
     } catch (err) {
-      console.error('Failed to fetch:', err.message); // Log any errors to the console
+      console.error('Failed to fetch:', err.message); 
     }
   };
 </script>
@@ -52,22 +52,23 @@
     <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
       <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-primary-400 to-secondary-800 opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
-    <div class="mx-auto py-80 sm:py-60 lg:py-80" >
-        <Heading tag="h1" class="mb-4 animate-slidein300  opacity-0" customSize="text-6xl font-extrabold  md:text-5xl lg:text-8xl">Welcome to my website</Heading>
-        <p class="text-lightParagraph dark:text-paragraph animate-slidein500 opacity-0">Hi, I'm Jamie, and this is my website about Gilbert. I build backend and frontend apps and run a Minecraft server.</p>
-        <div class="mt-10 flex items-center gap-x-6">
+    <div class="mx-auto py-90 sm:py-60 lg:py-80" >
+        <Heading tag="h1" class="mb-4 animate-slidein300  opacity-0" customSize="text-6xl font-extrabold  md:text-5xl lg:text-8xl">Welcome to my Website</Heading>
+        <p class="text-lightParagraph dark:text-paragraph animate-slidein500 opacity-0">Hi, I'm Jamie, and this is my website about Gilbert. I play video games, build backend and frontend apps, and run a Minecraft server.</p>
+        <div class="mt-10 flex items-center gap-x-6 animate-slidein700  opacity-0">
           <Button size='xl' class='btn bg-pink-500'>Join the Server</Button>
           <a href="/about" class="text-xl font-semibold text-Lightparagraph dark:text-paragraph">Learn more about me<span aria-hidden="true">→</span></a>
         </div>
     </div>
-    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-50rem)]" aria-hidden="true">
       <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-primary-400 to-secondary-800 opacity-40 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
   </div>
 </div>
-
+<br>
+<br>
 <h2 class="heading-primary"> What is Gilbert?</h2>
-<p class="text-lightParagraph dark:text-paragraph">Gilbert the Beanie Boo is a cute pink giraffe with purple spots. She is super soft to cuddle! 🐆🌈 Her birthday is August 23rd, loves to eat icecream. 💖✨ Here are some pictures of Gilbert:</p>
+<p class="text-lightParagraph dark:text-paragraph my-10">Gilbert the Beanie Boo is a cute pink giraffe with purple spots. She is super soft to cuddle! 🐆🌈 Her birthday is August 23rd, loves to eat icecream. 💖✨ Here are some pictures of Gilbert:</p>
 <div bind:this={container} class="flex overflow-x-auto max-w-full gap-5 scroll-snap-none no-scrollbar" >
     <div class={imgClass}>
         <enhanced:img src="$lib/images/1.jpg" alt="Gilbert 1" class="rounded-lg">
@@ -82,12 +83,13 @@
         <enhanced:img src="$lib/images/4.jpg" alt="Gilbert 4" class="rounded-lg">
     </div>
 </div>
+
 <h2 class="heading-primary">Join the Minecraft Server!</h2>
 <p class="text-lightParagraph dark:text-paragraph my-10">I'm currently working on Gilbert server season 2. I will update the server version to 1.21.4, but there are some mods that aren't updated yet.</p>
 <p class="text-lightParagraph dark:text-paragraph inline-block">Click here if you like Gilbert:</p> <Button on:click={fetchData} class='btn bg-pink-500'>I like Gilbert</Button>
 <p class="text-lightParagraph dark:text-paragraph">{count} people like gilbert</p>
 <h2 class="heading-primary"> Where is Gilbert? </h2>
-    <P class="text-2xl p-10" color="text-Lightparagraph dark:text-paragraph">Gilbert lives on my bed and she is very cute. Sometimes she visits my sisters bed to sleep sometimes. </P>
+    <p class="text-lightParagraph dark:text-paragraph">Gilbert lives on my bed and she is very cute. Sometimes she visits my sisters bed to sleep sometimes. </p>
     <Heading tag="h1" class="mb-4" customSize="text-4xl font-bold  md:text-5xl lg:text-6xl">Contact</Heading>
 <ul>
     <li class="text-4xl text-Lightparagraph dark:text-paragraph"><svg width="64px" height="64px" class='inline-block' viewBox="0 -28.5 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill="#5865F2" fill-rule="nonzero"> </path> </g> </g></svg> boowoof</li>
